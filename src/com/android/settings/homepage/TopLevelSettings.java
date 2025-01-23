xenxynon/penguin_set

Settings: Redesign top level settings
Change-Id: Ie2943674d425349496c22410be016393e9268fc1

src/com/android/settings/homepage/TopLevelSettings.java
@@ -37,6 +37,7 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.window.embedding.ActivityEmbeddingController;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -67,6 +68,29 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private boolean mScrollNeeded = true;
     private boolean mFirstStarted = true;
     private ActivityEmbeddingController mActivityEmbeddingController;
+
+    public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
+        "dashboard_tile_pref_com.oneplus.extras.DeviceSettings",
+        "dashboard_tile_pref_org.lineageos.settings.device",
+        "dashboard_tile_pref_org.lineageos.settings",
+        "dashboard_tile_pref_org.omnirom.devices",
+        "dashboard_tile_pref_org.omnirom.device",
+        "dashboard_tile_pref_com.poco.parts",
+        "dashboard_tile_pref_com.xiaomi.parts",
+        "dashboard_tile_pref_com.asus.zenparts"
+    };
+
+    public static final String[] CHANGE_LAYOUT_KEYS = {
+        "top_level_google",
+        "dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity",
+        "top_level_wellbeing",
+    };
+
+    public static final int[] LAYOUTS_N = {
+        R.layout.pengu_card_google,
+        R.layout.pengu_card_wellbeing,
+        R.layout.pengu_card_wellbeing
+    };
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -208,13 +232,19 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
-        int tintColor = Utils.getHomepageIconColor(getContext());
-        iteratePreferences(preference -> {
-            Drawable icon = preference.getIcon();
-            if (icon != null) {
-                icon.setTint(tintColor);
+        for (int i = 0; i < CHANGE_LAYOUT_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(LAYOUTS_N[i]);
             }
-        });
+        }
+        for (int i = 0; i < CHANGE_LAYOUT_AND_ORDER_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_AND_ORDER_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(R.layout.pengu_card_device);
+                preference.setOrder(12);
+            }
+        }
     }
 
     @Override

ProjectZenithAOSP/packages_apps_Settings

src/com/android/settings/homepage/TopLevelSettings.java
@@ -39,6 +39,7 @@ import androidx.preference.PreferenceGroup;
 import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.window.embedding.ActivityEmbeddingController;
+import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -71,6 +72,31 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private boolean mScrollNeeded = true;
     private boolean mFirstStarted = true;
     private ActivityEmbeddingController mActivityEmbeddingController;
+
+    public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
+        "dashboard_tile_pref_com.oneplus.extras.DeviceSettings",
+        "dashboard_tile_pref_org.lineageos.settings.device",
+        "dashboard_tile_pref_org.lineageos.settings",
+        "dashboard_tile_pref_org.omnirom.devices",
+        "dashboard_tile_pref_org.omnirom.device",
+        "dashboard_tile_pref_com.poco.parts",
+        "dashboard_tile_pref_com.xiaomi.parts",
+        "dashboard_tile_pref_com.asus.zenparts"
+    };
+
+    public static final String[] CHANGE_LAYOUT_KEYS = {
+        "top_level_google",
+        "dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity",
+        "top_level_wellbeing",
+        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity",        
+    };
+
+    public static final int[] LAYOUTS_N = {
+        R.layout.clover_card_google,
+        R.layout.clover_card_wellbeing,
+        R.layout.clover_card_wellbeing,
+        R.layout.clover_card_backup
+    };
 
     public TopLevelSettings() {
         final Bundle args = new Bundle();
@@ -216,13 +242,19 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         if (Flags.homepageRevamp()) {
             return;
         }
-        int tintColor = Utils.getHomepageIconColor(getContext());
-        iteratePreferences(preference -> {
-            Drawable icon = preference.getIcon();
-            if (icon != null) {
-                icon.setTint(tintColor);
+        for (int i = 0; i < CHANGE_LAYOUT_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(LAYOUTS_N[i]);
             }
-        });
+        }
+        for (int i = 0; i < CHANGE_LAYOUT_AND_ORDER_KEYS.length; i++) {
+            Preference preference = findPreference(CHANGE_LAYOUT_AND_ORDER_KEYS[i]);
+            if (preference != null){
+                preference.setLayoutResource(R.layout.clover_card_device);
+                preference.setOrder(12);
+            }
+        }
     }
 
     @Override